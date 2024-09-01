027b16f5a1440b680ef6c5c905607cb04d991fea
vappulla <142449928+vappulla@users.noreply.github.com>
2024-09-01 16:42:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,22 @@ function showSlides() {
     setTimeout(showSlides, 5000); 
 }
 
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    document.querySelector('.dropdown-content').classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
 
 
 
